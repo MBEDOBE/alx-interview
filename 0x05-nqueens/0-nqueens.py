6a958b2e353ea20463cf92b17c4097bb474fe9e2
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-08-01 10:31:37 +0000

    updated code

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -6,42 +6,42 @@ Determines all possible solutions to placing N non-attacking queens on an NxN ch
 import sys
 
 
-if len(sys.argv) > 2 or len(sys.argv) < 2:
+if len(sys.argv) != 2:
     print("Usage: nqueens N")
-    exit(1)
+    sys.exit(1)
 
 if not sys.argv[1].isdigit():
     print("N must be a number")
-    exit(1)
+    sys.exit(1)
 
 if int(sys.argv[1]) < 4:
     print("N must be at least 4")
-    exit(1)
+    sys.exit(1)
 
-n = int(sys.argv[1])
+N = int(sys.argv[1])
 
 
-def queens(n, i=0, a=[], b=[], c=[]):
-    """ find possible positions """
+def find_queen_positions(n, i=0, col_set=set(), diag_sum_set=set(), diag_diff_set=set()):
+    """Find possible positions for the queens."""
     if i < n:
         for j in range(n):
-            if j not in a and i + j not in b and i - j not in c:
-                yield from queens(n, i + 1, a + [j], b + [i + j], c + [i - j])
+            if j not in col_set and i + j not in diag_sum_set and i - j not in diag_diff_set:
+                yield from find_queen_positions(n, i + 1, col_set | {j}, diag_sum_set | {i + j}, diag_diff_set | {i - j})
     else:
-        yield a
+        yield list(col_set)
 
 
 def solve(n):
-    """ solve """
-    k = []
-    i = 0
-    for solution in queens(n, 0):
-        for s in solution:
-            k.append([i, s])
-            i += 1
-        print(k)
+    """Solve and print the solutions."""
+    solution_count = 0
+    for solution in find_queen_positions(n, 0):
         k = []
-        i = 0
+        for i, col in enumerate(solution):
+            k.append([i, col])
+        print(k)
+        solution_count += 1
+
+    print(f"Total solutions: {solution_count}")
 
 
-solve(n)
+solve(N)

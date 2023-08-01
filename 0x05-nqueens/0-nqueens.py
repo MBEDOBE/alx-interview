b7e770336042e4439c8227468d041e87b601507b
D. Mbedobe Kunji <danielbushbaby@gmail.com>
2023-08-01 10:16:21 +0000

    readme created

--- 0x05-nqueens/0-nqueens.py
+++ 0x05-nqueens/0-nqueens.py
@@ -1,5 +1,8 @@
+#!/usr/bin/python3
+"""Solves the N-queens puzzle.
+Determines all possible solutions to placing N
+N non-attacking queens on an NxN chessboard."""
 import sys
-
 
 def is_safe(board, row, col, N):
     # Check if there is a queen in the same column
